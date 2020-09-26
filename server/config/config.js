aecceb8f12ba927545d78e10a37076150f602973
Diego <diego.a.leon24@gmail.com>
2020-09-26 00:25:29 -0500

    variable de entorno de mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let databaseConnection = process.env.NODE_ENV === 'dev' ?
     'mongodb://root:123@localhost:27017/cafe?authSource=admin' :
-    'mongodb+srv://leon:dQAwCzJCRIJsBuGK@cluster0.yiujd.azure.mongodb.net/cafe';
+    process.env.MONGO_URI;
 
 process.env.URL_DB = databaseConnection;
